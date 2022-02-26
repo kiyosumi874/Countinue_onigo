61c7b70a02d41c59a3acf8e584431fd312c2dd5e
yanoharuto <8bitharuto@gmail.com>
2022-02-26 22:45:17 +0900

    enemy
    追いかけられる敵のスクリプトをとりあえず作った。ナビゲーションの勉強してから動くところを作る

---
+++ Assets/PlayScene/C#Script/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 追いかけられる敵
+/// </summary> 2022/2/26 by YanoHaruto
+///　2/26 とりあえず倒される処理を作った
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private string DefeatTag;//倒される原因のタグ
+
+    /// <summary>
+    /// 倒される
+    /// </summary>
+    /// <param name="TagName">倒される原因のタグ</param>
+    private void Down(string TagName)
+    {
+        if (TagName == DefeatTag)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Down(other.tag);
+    }
+
+}
