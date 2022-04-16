e85aba333397d7b5617bba76f847887e7f506bd0
yanoharuto <8bitharuto@gmail.com>
2022-04-16 12:11:03 +0900

    SceneChange

--- Assets/PlayScene/C#Script/Scene/PlayScene.cs
+++ Assets/PlayScene/C#Script/Scene/PlayScene.cs
@@ -1,8 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayScene : SceneBase
 {
-    [SerializeField] List<EscapeEnemy> mEscapeEnemy = new List<EscapeEnemy>();
+    [SerializeField] private int mEnemyMax;
+    [SerializeField] private float mTimeLimit;
+    private int mEnemyCount;
+    private float mTime;
+    public void mEnemyCounter()
+    {
+        mEnemyCount++;
+    }
+    private void Timer()
+    {
+        mTime += Time.deltaTime;
+        if (mTime > mTimeLimit) 
+        {
+            SceneChange();
+        }
+    }
+    private void Start()
+    {
+        mEnemyCount = 0;
+        mTime = 0;
+    }
+    private void Update()
+    {
+        if(mEnemyCount==mEnemyMax)
+        {
+            SceneChange();
+        }
+        Timer();
+    }
 }
